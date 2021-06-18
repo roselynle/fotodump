d337735bf29b7a6a5c6fbd72c0f0943961f06f26
Roselyn Le <roselynle95@gmail.com>
2021-06-18 20:41:37 +0100

    render the views

--- photosharing/photos/views.py
+++ photosharing/photos/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def gallery(request):
+    return render(request, 'photos/gallery.html')
+
+def view(request):
+    return render(request, 'photos/photo.html')
+
+def add(request):
+    return render(request, 'photos/addphoto.html')

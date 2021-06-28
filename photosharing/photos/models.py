4cbbf1e8dc1759fdd3e4f1792a02485eae1fdd96
Roselyn Le <roselynle95@gmail.com>
2021-06-28 18:34:31 +0100

    create models

--- photosharing/photos/models.py
+++ photosharing/photos/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+      return self.name
+
+class Photo(models.Model):
+    description = models.CharField(max_length=500)
+
+    def __str__(self):
+      return self.name

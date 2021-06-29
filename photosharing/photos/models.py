5e2243c3c04e936c34eaf0fbe09c41133c37067a
Roselyn Le <roselynle95@gmail.com>
2021-06-29 13:53:30 +0100

    add validations to models

--- photosharing/photos/models.py
+++ photosharing/photos/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 
-# Create your models here.
-
 class Category(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, null=False, blank=False)
 
     def __str__(self):
-      return self.name
+        return self.name
 
 class Photo(models.Model):
-    description = models.CharField(max_length=500)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+    image = models.ImageField(null=False, blank=False)
+    description = models.TextField()
 
     def __str__(self):
-      return self.name
+        return self.description
